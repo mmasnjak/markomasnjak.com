a4734b93f407bfc11ce7e09c1372325a390e7f25
Marko Masnjak <marko@dscout.com>
2015-09-02 20:04:41 -0500

    Add submodules

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -1,1 +1,15 @@
+$.jribbble.setToken('5eda91fcb18044ffd52459355dd1ba936701f193e6aeff1f9af256fefcc0faa1');
+$.jribbble.shots('teams').then(function(shots) {
+      var html = [];
+
+      shots.forEach(function(shot) {
+        // See the Dribbble docs for all available shot properties.
+        html.push('<div class="grid-content">');
+        html.push('<a href="' + shot.html_url + '">');
+        html.push('<img src="' + shot.images.normal + '">');
+        html.push('</a></div>');
+      });
+
+      $('#dribbble-data').html(html.join(''));
+    });
 //= require_tree .
