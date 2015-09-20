05c7467059b568569bbdcd2369c60c2b273f614f
Marko Masnjak <marko@dscout.com>
2015-09-19 22:22:29 -0500

    Start mobile layout

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -1,20 +1,19 @@
 $.jribbble.setToken('5eda91fcb18044ffd52459355dd1ba936701f193e6aeff1f9af256fefcc0faa1');
 
-$.jribbble.users('m_masnjak').shots({'per_page' : 4}).then(function(shots) {
-      var html = [];
+$.jribbble.users('m_masnjak').shots({'per_page' : 8}).then(function(shots) {
+  var html = [];
 
-      shots.forEach(function(shot) {
-        // See the Dribbble docs for all available shot properties.
-        html.push('<article class="dribbble-shot">');
-        html.push('<a href="' + shot.html_url + '"><img src="' + shot.images.hidpi + '"></a>');
-        html.push('<h2>' + shot.title + '</h2>');
-        html.push(shot.description);
-        html.push('<div class="meta">Views: ' + shot.views_count + ' Buckets: ' + shot.buckets_count + ' Likes: ' + shot.likes_count + '</div>');
-        html.push('</article>');
-      });
+  shots.forEach(function(shot) {
+    html.push('<div class="thumbnail-item">');
+    html.push('<a href="' + shot.html_url + '" target="_blank">');
+    html.push('<img src="' + shot.images.normal + '" style="border-bottom: 1px solid rgba(0,0,0,.12);">');
+    html.push('<strong>' + shot.title + '</strong>');
+    html.push('<small class="meta-info">' + getTimestamp(shot.created_at) + '</small>');
+    html.push('</a></div>');
+  });
 
-      $('#dribbble-data').html(html.join(''));
-    });
+  $('#dribbble-data').html(html.join(''));
+});
 
 twitterFetcher.fetch({
   'id'              : '259728338522873856',
@@ -24,4 +23,7 @@ twitterFetcher.fetch({
   'enableLinks'     : true,
   'showPermalinks'  : false
 });
-//= require_tree .
+
+function getTimestamp(d) {
+  return new Date(d).toLocaleDateString();
+}
