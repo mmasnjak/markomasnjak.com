8a894547e4086713114a4964309b49079fd57729
Marko Masnjak <marko@dscout.com>
2015-11-02 22:33:18 -0600

    Housekeeping

--- config.rb
+++ config.rb
@@ -1,57 +1,21 @@
-###
-# Compass
-###
-
 # Change Compass configuration
 compass_config do |config|
   config.output_style = :compact
 end
 
-###
 # Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
 # Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'static/css'
 set :js_dir, 'static/js'
 set :images_dir, 'static/images'
 
 # Development-specific configuration
 configure :development do
+
   # Reload browser automatically when files change
   activate :livereload
 end
@@ -70,10 +34,4 @@ configure :build do
 
   # Enable cache buster
   activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
